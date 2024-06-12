0c1ca6d53e66bbd8e1828daa877a7fc91d56e2db
nerggg <13522147@std.stei.itb.ac.id>
2024-06-12 10:09:04 +0700

    feat: video preparation

--- main.py
+++ main.py
@@ -75,34 +75,34 @@ class Node:
             child.make_tree(depth-1, child_count)
             del move_temp
 
-def minimax(node, depth, maximizing, prev_value):
+def minimax(node, depth, maximizing):
     if depth == 0 or len(node.child) == 0:
         return node
     
     if maximizing: # white turn
         if node.eval['type'] == 'cp':
-            extreme_node = minimax(node.child[0], depth-1, False, prev_value)
+            extreme_node = minimax(node.child[0], depth-1, False)
             for i in range (1, len(node.child)):
-                minimax_node = minimax(node.child[i], depth-1, False, prev_value)
+                minimax_node = minimax(node.child[i], depth-1, False)
                 if (minimax_node.eval['value'] > extreme_node.eval['value']):
                     extreme_node = minimax_node
         else: # white mating
-            extreme_node = minimax(node.child[0], depth-1, False, prev_value)
+            extreme_node = minimax(node.child[0], depth-1, False)
             for i in range (1, len(node.child)):
-                minimax_node = minimax(node.child[i], depth-1, False, prev_value)
+                minimax_node = minimax(node.child[i], depth-1, False)
                 if (minimax_node.eval['value'] < extreme_node.eval['value']):
                     extreme_node = minimax_node
     else: # black turn
         if node.eval['type'] == 'cp':
-            extreme_node = minimax(node.child[0], depth-1, True, prev_value)
+            extreme_node = minimax(node.child[0], depth-1, True)
             for i in range (1, len(node.child)):
-                minimax_node = minimax(node.child[i], depth-1, True, prev_value)
+                minimax_node = minimax(node.child[i], depth-1, True)
                 if (minimax_node.eval['value'] < extreme_node.eval['value']):
                     extreme_node = minimax_node
         else: # black mating
-            extreme_node = minimax(node.child[0], depth-1, True, prev_value)
+            extreme_node = minimax(node.child[0], depth-1, True)
             for i in range (1, len(node.child)):
-                minimax_node = minimax(node.child[i], depth-1, True, prev_value)
+                minimax_node = minimax(node.child[i], depth-1, True)
                 if (minimax_node.eval['value'] > extreme_node.eval['value']):
                     extreme_node = minimax_node
     # print("yg di return itu " + str(extreme_node.move) + " dengan value " + str(extreme_node.eval['value']))
@@ -149,15 +149,11 @@ def print_board(sf):
             ascii_board += fen_to_ascii.get(board[i], board[i])
     return ascii_board
 
-# fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-fen = "4K3/4P1k1/8/8/8/8/7R/5r2 b - - 0 1"
-# fen = "4K3/4P1k1/8/8/8/8/7R/r7 w - - 1 2"
-# fen = "4K3/4P1k1/8/8/8/8/6R1/r7 b - - 2 2"
-# fen = "4K3/4P3/5k2/8/8/8/6R1/r7 w - - 3 3"
+# fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" # initial position fen
+fen = "4K3/4P1k1/8/8/8/8/7R/5r2 b - - 0 1" # fen used in the document
 
-# fen = "8/8/4K1R1/8/8/7k/8/8 w - - 18 20"
 # SINGLE PRINTING
-# stockfish = Stockfish(path="./stockfish/stockfish-windows-x86-64-avx2.exe") # depth defaultnya 15
+# stockfish = Stockfish(path="./stockfish/stockfish-windows-x86-64-avx2.exe")
 # stockfish.set_fen_position(fen)
 # root = Node(stockfish, [])
 # root.make_tree(3, 2) # depth, child_count
@@ -168,21 +164,18 @@ fen = "4K3/4P1k1/8/8/8/8/7R/5r2 b - - 0 1"
 # print(stockfish.get_fen_position())
 
 # MAIN GAME
-white = False
-prev_value = 0
+white = False # use True if white is to play the turn
 while True:
-    stockfish = Stockfish(path="./stockfish/stockfish-windows-x86-64-avx2.exe") # depth defaultnya 15
+    stockfish = Stockfish(path="./stockfish/stockfish-windows-x86-64-avx2.exe")
     stockfish.set_fen_position(fen)
     root = Node(stockfish, [])
-    root.make_tree(3, 2)
-    move = minimax(root, 3, white, prev_value).move
+    root.make_tree(3, 2) # depth, child count
+    move = minimax(root, 3, white).move
     stockfish.make_moves_from_current_position([move[0]])
     print(print_board(stockfish))
     print(move)
     fen = stockfish.get_fen_position()
     print(fen)
-
-    prev_value = root.eval['value']
 
     if white:
         white = False
